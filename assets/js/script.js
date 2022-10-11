b749c7251591da76feee2d329c4717b7e0af1732
Alice Shao <a1ice_shao@yahoo.com>
2022-10-11 13:58:55 -0400

    added js folder and js pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Dependencies
+// City input
+// Sumbit button
+// Modal trigger (probably a button to open the menu)
+// Place to display events
+// Place to display saved events
+
+// Data
+// User location
+// Current date and time
+// NASA API
+
+// Function
+// getAPI
+// Grab user location
+// Autocomplete (optional)
+
+// User interaction
+// Textbox for user to input location
+    // Submit button
+    // Using a Modal?
+// Button for user to save event
+// Button for user to remove saved event
